56f49c4b9d2d76a5b1ffc18c81977dbc6914e932
zapatillaasesina <lofljordi@gmail.com>
2016-06-01 12:39:23 +0200

    documentació correcta

--- Main_dolent.java
+++ Main_dolent.java
@@ -1,44 +1,40 @@
-
-
 import java.util.Scanner;
 import java.util.Random;
 
 public class Main_dolent {
 	/**
-	 * @author Jordi Garcia
-	 * @param paraules: total de paraules que pot conté el joc.
-	 * @param index: valor per determinar quina sera la paraula amb la qual jugarem.
-	 * @param p: lletres de la paraules representadas com "_"
+	 * @param paraules conté totes les paraules que poden surtir al joc.
+	 * @param index numero per seleccionar la paraula amb la qual es jugara.
+	 * @param p visualitza la paraula.
 	 */
-	
-
-    static protected String paraules[] = {"hola","programació","casa"}; 
+    static protected String paraules[] = {"hola","programació","casa"};
     static protected int index;
     static protected String p;
 
     public static void main(String[] args) {
-    	/**
-    	 * @param num_errors: numero total d'errors que ha comés el jugador.
-    	 */
-    	int num_errors = 0;
         System.out.println("Comença el joc!");
         /**
-         * es genera un numero aleatori per seleccionar la paraula per jugar i el representa.
+         * es genera un numero aleatori al parametre "index" per seleccionar
+         * la paraula per jugar i la representa gracies al parametre "p",
+         * que per cada lletra de la paraula es igual a "_".
          */
+
         Random rand = new Random();
         index = rand.nextInt(3);
         for (int i=0; i<paraules[index].length(); i++) {
-            p = "_";
+            p += "_";
         }
 
         System.out.println(p);
 
         Scanner reader = new Scanner(System.in);
-
-        
         /**
-         * comptador de errors, cada cop que es falla una lletra augmenta el comptador, en arrivar a 5 has perdut.
+         * Comptador dels errors per cada lletra incorrecta, en arribar a 5
+         * el joc s'acaba automàticament.
+         * @param num_errors compta el nombre d'errors del jugador
          */
+        int num_errors = 0;
+
         while (!p.equals(paraules[index]) && num_errors < 5) {
             if (!nova_lletra(reader.next().charAt(0))) {
                 num_errors++;
@@ -47,10 +43,12 @@ public class Main_dolent {
         }
    }
     /**
-     * classe per a que el jugador introdueixi una lletra i comprobar si es troba a la paraula o no. si hi es, canvia "_" per la lletra.
+     * Classe per a que el jugador introdueixi una lletra i comprobar si es
+     * troba a la paraula o no. si hi es, canvia "_" per la lletra.
      * @param nova: es la seguent lletra introduida per el jugador.
      * @return retorna la paraula 
      */
+
     static public boolean nova_lletra(char nova) {
         boolean trobada = false;
         String paraula_old = p;
